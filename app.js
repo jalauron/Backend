aad68982b75a40727b226e723e5d930096d57f2e
jalauron <jalauron@gbox.ncf.edu.ph>
2024-10-18 21:35:13 +0800

    E

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/api/departments', deptRoute1);
 app.use('/api/courses', coursesRoute1);
 app.use('/api/students', studentsRoute1);
 
-const PORT = process.env.PORT;
+const PORT = 5000;
 
 app.listen(PORT, () => {
     console.log('Server is running on port 5000');
